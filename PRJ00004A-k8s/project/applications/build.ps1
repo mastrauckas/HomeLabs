bf6734e84aee6f35bf378d9b3bcce8849a4214f7
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2024-04-28 23:30:01 -0400

    Adding deployments.

--- PRJ00004A-k8s/project/applications/build.ps1
+++ PRJ00004A-k8s/project/applications/build.ps1
@@ -1,2 +1,12 @@
 cd showversionsv6
 dotnet publish -c Release -o ./build
+docker build -t showversionsv6:v1.0.0 -f Dockerfile .
+docker tag showversionsv6:v1.0.0 localhost:8000/showversionsv6:v1.0.0
+docker push localhost:8000/showversionsv6:v1.0.0
+cd ..
+cd showversionsv8
+dotnet publish -c Release -o ./build
+docker build -t showversionsv8:v1.0.0 -f Dockerfile .
+docker tag showversionsv8:v1.0.0 localhost:8000/showversionsv8:v1.0.0
+docker push localhost:8000/showversionsv6:v1.0.0
+cd ..
