de3a85ea9a3185f629d9edc99ee30b37e064babb
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2024-07-06 10:08:33 -0400

    Adding build files.

--- docker/sql-server-select/building/build.ps1
+++ docker/sql-server-select/building/build.ps1
@@ -1,7 +1,10 @@
-dotnet publish -c Release -o ./build
-docker build -t printdotnet8:v1.0 -f Dockerfile .
-docker build -t printdotnet8:latest -f Dockerfile .
-docker tag printdotnet8:v1.0 mastrauckas/printdotnet8:v1.0
-docker tag printdotnet8:latest mastrauckas/printdotnet8:latest
-docker push mastrauckas/printdotnet8:v1.0
-docker push mastrauckas/printdotnet8:latest
+cd ../src/
+dotnet publish -c Release -o ../building/build
+cd ../building
+
+docker build -t sql-server-select:v1.0 -f Dockerfile .
+docker build -t sql-server-select:latest -f Dockerfile .
+docker tag sql-server-select:v1.0 mastrauckas/sql-server-select:v1.0
+docker tag sql-server-select:latest mastrauckas/sql-server-select:latest
+docker push mastrauckas/sql-server-select:v1.0
+docker push mastrauckas/sql-server-select:latest
