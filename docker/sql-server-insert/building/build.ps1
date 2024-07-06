5cfbe4c2424d553e83e250f4fe3e260595a8fefd
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2024-07-06 13:16:50 -0400

    Changes.

--- docker/sql-server-insert/building/build.ps1
+++ docker/sql-server-insert/building/build.ps1
@@ -2,9 +2,9 @@ cd ../src/
 dotnet publish -c Release -o ../building/build
 cd ../building
 
-docker build -t sql-server-select:v1.0 -f Dockerfile .
-docker build -t sql-server-select:latest -f Dockerfile .
-docker tag sql-server-select:v1.0 mastrauckas/sql-server-select:v1.0
-docker tag sql-server-select:latest mastrauckas/sql-server-select:latest
-docker push mastrauckas/sql-server-select:v1.0
-docker push mastrauckas/sql-server-select:latest
+docker build -t sql-server-insert:v1.0 -f Dockerfile .
+docker build -t sql-server-insert:latest -f Dockerfile .
+docker tag sql-server-insert:v1.0 mastrauckas/sql-server-insert:v1.0
+docker tag sql-server-insert:latest mastrauckas/sql-server-insert:latest
+docker push mastrauckas/sql-server-insert:v1.0
+docker push mastrauckas/sql-server-insert:latest
