0f31671377dd15191151c858563d3a14f88c823a
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2024-08-03 13:16:30 -0400

    Adding new application.

---
+++ docker/dotnet8-environment-variables/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+     .AddJsonFile("appsettings.json")
+     .AddUserSecrets<Program>()
+     .AddEnvironmentVariables();
+
+var configuration = builder.Build();
+
+var table = configuration.GetSection("Table").Get<Table>();
+
+ArgumentNullException.ThrowIfNull(table);
+
+Console.WriteLine($"Table name is: {table.TableName}.");
+
+foreach (var foreignKeyTable in table.ForeignKeyTables)
+{
+    Console.WriteLine($"Foreign Key Table for {table.TableName}: {foreignKeyTable.TableName}.");
+}
+
+Console.WriteLine();
+
+var virtueScriptConnectionString = configuration.GetConnectionString("VirtueScriptConnectionString");
+var virtueScriptImportConnectionString = configuration.GetConnectionString("VirtueScriptImportConnectionString");
+var appInsightsConnectionString = configuration.GetConnectionString("AppInsightsConnectionString");
+var cosmosConnectionString = configuration.GetConnectionString("CosmosConnectionString");
+
+Console.WriteLine($"VirtueScriptConnectionString: {virtueScriptConnectionString}.");
+Console.WriteLine($"VirtueScriptImportConnectionString: {virtueScriptImportConnectionString}.");
+Console.WriteLine($"AppInsightsConnectionString: {appInsightsConnectionString}.");
+Console.WriteLine($"CosmosConnectionString: {cosmosConnectionString}.");
+
+var howLongToSleepFor = configuration.GetValue<int>("HowLongToSleepForInSeconds");
+
+Console.WriteLine($"Sleep for {howLongToSleepFor} seconds.");
+
+Thread.Sleep(howLongToSleepFor * 1000);
+
+Console.WriteLine($"Application is now ending.");
+
+return;
+
+public class ForeignKeyTable
+{
+    public required string TableName { get; set; }
+}
+
+public class Table
+{
+    public required string TableName { get; set; }
+    public required List<ForeignKeyTable> ForeignKeyTables { get; set; }
+
+}
