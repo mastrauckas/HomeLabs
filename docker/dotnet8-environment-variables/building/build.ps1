a16c2189c63d8b990ded2c9d414b4d618483100b
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2024-08-03 13:37:08 -0400

    Adding pieces.

---
+++ docker/dotnet8-environment-variables/building/build.ps1
@@ -0,0 +1,10 @@
+# cd ../src/
+# dotnet publish -c Release -o ../building/build
+# cd ../building
+
+docker build -t dotnet8-environment-variables:v1.0 -f Dockerfile .
+docker build -t dotnet8-environment-variables:latest -f Dockerfile .
+docker tag dotnet8-environment-variables:v1.0 mastrauckas/dotnet8-environment-variables:v1.0
+docker tag dotnet8-environment-variables:latest mastrauckas/dotnet8-environment-variables:latest
+docker push mastrauckas/dotnet8-environment-variables:v1.0
+docker push mastrauckas/dotnet8-environment-variables:latest
