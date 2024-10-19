521e5b1621acf65cbf1bfcc189ed4003ee73ce77
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2024-10-19 10:48:39 -0400

    Add logging.

---
+++ azure/aks/aks-with-pod-workspace-logging/deployment.ps1
@@ -0,0 +1,15 @@
+$region = 'eastus'
+$resourceGroup = "k8s-testing-cluster-rg"
+
+az group create `
+    --resource-group $resourceGroup `
+    --location $region;
+
+az deployment group create `
+    --name k8s-testing-deployment `
+    --template-file ./main.bicep `
+    --parameters ./parameters/main.bicepparam `
+    --parameters region=$region `
+    --resource-group $resourceGroup `
+    --debug `
+    --verbose;
